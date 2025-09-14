f11116314469ebe5dec0df919c1ed8d270db9d3b
ntthuyha14 <ntthuyha14@gmail.com>
2025-09-14 16:12:23 +0700

    TC_RF_008

---
+++ src/test/java/tutorialsninja/register/TC_RF_008.java
@@ -0,0 +1,54 @@
+package tutorialsninja.register;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Date;
+
+public class TC_RF_008 {
+@Test
+    public void verifyDifferentPasscodeConfirm(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo/");
+
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+
+        driver.findElement(By.id("input-firstname")).sendKeys("Arun");
+        driver.findElement(By.id("input-lastname")).sendKeys("Motoori");
+        driver.findElement(By.id("input-email")).sendKeys(generateEmail());
+        driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+        driver.findElement(By.id("input-password")).sendKeys("12345");
+        driver.findElement(By.id("input-confirm")).sendKeys("abcdf");
+        driver.findElement(By.xpath("//input[@name='newsletter'][@value='0']")).isSelected();
+        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+        String expectedConfirmPasscodeWarning = "Password confirmation does not match password!";
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='text-danger']")).getText(), expectedConfirmPasscodeWarning);
+        Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+
+
+
+
+    }
+    public static String generateEmail(){
+        Date date = new Date();
+        String dateString = date.toString();
+        String noSpaceDateString = dateString.replaceAll("\\s", "");
+        String noSpaceAndColonstateString = noSpaceDateString.replaceAll("\\:", "");
+        String emailWithTimeStamp = noSpaceAndColonstateString + "@gmail.com";
+        System.out.println(emailWithTimeStamp);
+        return emailWithTimeStamp;
+    }
+
+}
